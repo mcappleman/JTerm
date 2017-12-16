808b922752a29aa8785e36f03321da3b73269fba
DevAjax <32197112+DevAjax@users.noreply.github.com>
2017-12-16 22:30:48 +0100

    created prototype of ssh

---
+++ src/main/java/jterm/command/SSH.java
@@ -0,0 +1,121 @@
+package jterm.command;
+
+import jterm.JTerm;
+import jterm.io.output.TextColor;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.jcraft.jsch.*;
+
+import java.io.InputStream;
+import java.util.logging.Level;
+
+public class SSH {
+    private static final Logger LOGGER = Logger.getLogger(SSH.class.getName());
+    private static JSch jschSSHChannel;
+    private static String strUserName;
+    private static String strConnectionIP;
+    private static int intConnectionPort;
+    private static String strPassword;
+    private static Session sesConnection;
+    private static String errorMessage;
+
+    @Command(name = "ssh", minOptions = 1, syntax = "ssh user@connectionIP")
+    public static void makeSSH(List<String> options) {
+        String[] arg = options.get(0).split("@");
+        strUserName = arg[0];
+        strConnectionIP = arg[1];
+
+        try {
+            JTerm.out.print(TextColor.INFO, options.get(0) + "'s password: ");
+            strPassword = JTerm.userInput.readLine();
+            intConnectionPort=22;
+            jschSSHChannel = new JSch();
+            try {                                                   //TODO
+                jschSSHChannel.setKnownHosts("");
+            } catch (JSchException jschX) {
+                logError(jschX.getMessage());
+            }
+
+            String errorMessage = SSH.connect();
+            if (errorMessage != null) {
+                JTerm.out.println(TextColor.INFO,errorMessage);
+            }
+
+        }catch (IOException e){     //handle exception TODO
+
+
+        }
+
+    }
+
+    public static String connect() {
+        String errorMessage = null;
+
+        try {
+            sesConnection = jschSSHChannel.getSession(strUserName,
+                    strConnectionIP, intConnectionPort);
+            sesConnection.setPassword(strPassword);
+            // UNCOMMENT THIS FOR TESTING PURPOSES, BUT DO NOT USE IN PRODUCTION
+            sesConnection.setConfig("StrictHostKeyChecking", "no");
+            sesConnection.connect(60000);
+
+        } catch (JSchException jschX) {
+            errorMessage = jschX.getMessage();
+        }
+
+        return errorMessage;
+    }
+
+    private static String logError(String errorMessage) {
+        if (errorMessage != null) {
+            LOGGER.log(Level.SEVERE, "{0}:{1} - {2}",
+                    new Object[]{strConnectionIP, intConnectionPort, errorMessage});
+        }
+
+        return errorMessage;
+    }
+
+    private static String logWarning(String warnMessage) {
+        if (warnMessage != null) {
+            LOGGER.log(Level.WARNING, "{0}:{1} - {2}",
+                    new Object[]{strConnectionIP, intConnectionPort, warnMessage});
+        }
+
+        return warnMessage;
+    }
+
+    public static String sendCommand(String command) {
+        StringBuilder outputBuffer = new StringBuilder();
+
+        try {
+            Channel channel = sesConnection.openChannel("exec");
+            ((ChannelExec) channel).setCommand(command);
+            InputStream commandOutput = channel.getInputStream();
+            channel.connect();
+            int readByte = commandOutput.read();
+
+            while (readByte != 0xffffffff) {
+                outputBuffer.append((char) readByte);
+                readByte = commandOutput.read();
+            }
+            System.out.println(outputBuffer); //@ssh response
+            channel.disconnect();
+        } catch (IOException ioX) {
+            logWarning(ioX.getMessage());
+            return null;
+        } catch (JSchException jschX) {
+            logWarning(jschX.getMessage());
+            return null;
+        }
+
+        return outputBuffer.toString();
+    }
+
+    public static void close() {
+        sesConnection.disconnect();
+    }
+
+}
